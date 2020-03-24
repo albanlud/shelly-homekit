7983f478811ecf7e0ba2e32180f86331a0620b37
I058323 <alban.ludewig@sap.com>
2020-03-24 12:30:34 +0100

    delete build forlder from git

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20200323-103501";
-const char *build_timestamp = "2020-03-23T10:35:01Z";
-const char *build_version = "1.4.0";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20200323-103501/2.17.0-gc31a745";
-const char *mg_build_timestamp = "2020-03-23T10:35:01Z";
-const char *mg_build_version = "2.17.0";
